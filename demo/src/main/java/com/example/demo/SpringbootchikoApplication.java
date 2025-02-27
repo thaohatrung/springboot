319dff1cf070dc67a27f14cef9d301d2129cad8f
anhtukgt <nguyenanhtuqn2707l@gmail.com>
2025-02-27 13:37:27 +0700

    Initial Spring Boot project

---
+++ demo/src/main/java/com/example/demo/SpringbootchikoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootchikoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootchikoApplication.class, args);
+	}
+
+}
